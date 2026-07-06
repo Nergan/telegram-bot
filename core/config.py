799d74fb0377b66a10eb5f6c81faa149e02c0d89
Nergan <mak.kriuchkov@yandex.ru>
2026-07-06 16:37:27 +0700

    set speed 0.8 / 0.1

--- core/config.py
+++ core/config.py
@@ -8,8 +8,8 @@ MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "dating_bot_secure_path_abc123")
 
-DATA_SPEED_LIMIT = 0.3 # optimal - 0.8
-KEYBOARD_SPEED_LIMIT = 0.2 # optimal - 0.5
+DATA_SPEED_LIMIT = 0.8 # optimal - 0.8
+KEYBOARD_SPEED_LIMIT = 0.1 # optimal - 0.5
 
 BOT_COMMANDS = [
     ("start", "Start or restart the bot"),
