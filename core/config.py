eb4c641973765ea670154297089383032d18ebac
Nergan <mak.kriuchkov@yandex.ru>
2026-07-06 16:33:17 +0700

    edit speed

--- core/config.py
+++ core/config.py
@@ -8,7 +8,7 @@ MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "dating_bot_secure_path_abc123")
 
-DATA_SPEED_LIMIT = 0.8 # optimal - 0.8
+DATA_SPEED_LIMIT = 0.3 # optimal - 0.8
 KEYBOARD_SPEED_LIMIT = 0.2 # optimal - 0.5
 
 BOT_COMMANDS = [
